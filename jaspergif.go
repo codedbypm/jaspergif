e48873b0ddcdfbbb4979aabccb300f4ea83c26d0
Paolo Moroni <paolom@backbase.com>
2020-05-31 20:22:22 +0200

    First commit

---
+++ jaspergif.go
@@ -0,0 +1,27 @@
+// Package p contains an HTTP Cloud Function.
+package jaspergif
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// HelloWorld prints the JSON encoded "message" field in the body
+// of the request or "Hello, World!" if there isn't one.
+func Jaspergif(w http.ResponseWriter, r *http.Request) {
+	var response struct {
+		Challenge string `json:"challenge"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&response); err != nil {
+		fmt.Fprint(w, "ukMkofynvX8SiGDxp1APZBXG7TorVFVqjaxKzsPnjJY64XIzl7TT")
+		return
+	}
+	if response.Challenge == "" {
+		fmt.Fprint(w, "ukMkofynvX8SiGDxp1APZBXG7TorVFVqjaxKzsPnjJY64XIzl7TT")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(response.Challenge))
+}
