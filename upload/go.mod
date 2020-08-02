387ae86e0c8a709fa55f46105ce569fd22932a3d
Paolo Moroni <paolo@codedby.pm>
2020-08-03 01:45:53 +0200

    Finish upload

--- upload/go.mod
+++ upload/go.mod
@@ -5,10 +5,12 @@ go 1.13
 require (
 	cloud.google.com/go/firestore v1.2.0
 	cloud.google.com/go/pubsub v1.3.1
+	cloud.google.com/go/storage v1.10.0
 	github.com/GoogleCloudPlatform/functions-framework-go v1.0.1
 	github.com/codedbypm/jaspergify/entry v0.0.0-20200613214419-6b18f505a53a
-	github.com/codedbypm/jaspergify/log v0.0.0-20200802194024-9d3a44b9d7cd
-	github.com/codedbypm/jaspergify/model v0.0.0-20200802194024-9d3a44b9d7cd
+	github.com/codedbypm/jaspergify/log v0.0.0-20200802213224-04944486f9a0
+	github.com/codedbypm/jaspergify/model v0.0.0-20200802213224-04944486f9a0
 	gocv.io/x/gocv v0.24.0
 	golang.org/x/sys v0.0.0-20200802091954-4b90ce9b60b3 // indirect
+	golang.org/x/tools v0.0.0-20200731060945-b5fad4ed8dd6 // indirect
 )
--- upload/upload.go
+++ upload/upload.go
@@ -3,14 +3,57 @@ package upload
 
 import (
 	"context"
+	"net/http"
+	"path"
+	"time"
 
+	"cloud.google.com/go/storage"
+	"github.com/codedbypm/jaspergify/log"
 	"github.com/codedbypm/jaspergify/model"
 )
 
 // OnFetchGif is the new awesome thing
 func OnFetchGif(ctx context.Context, e model.FirestoreGifEvent) error {
 
-	// var gifURL = e.Value.Fields.URL.Value
+	var gifURL = e.Value.Fields.URL.Value
 
+	// Get the data
+	resp, err := http.Get(gifURL)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	var data []byte
+	resp.Body.Read(data)
+
+	return uploadFile(data, "jaspergify-gifs-mp4", path.Base(gifURL))
+}
+
+// uploadFile uploads an object.
+func uploadFile(data []byte, bucketName, objectName string) error {
+	// object := "object-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	writer := client.Bucket(bucketName).Object(objectName).NewWriter(ctx)
+	if _, err := writer.Write(data); err != nil {
+		log.Error(err)
+		return err
+	}
+	if err := writer.Close(); err != nil {
+		log.Error(err)
+		return err
+	}
 	return nil
 }
